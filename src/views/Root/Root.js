b03c43f30af89c9f078f99170d34c4cb52a2f7f3
Patryk <pacynek93@gmail.com>
2021-01-14 12:28:17 +0100

    project setup and adding button

---
+++ src/views/Root/Root.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from '../../components/Button/Button';
+
+const Root = () => (
+  <div>
+    <h1>Hello Blyat</h1>
+    <Button>CLOSE / SAVE</Button>
+    <Button secondary>Remove</Button>
+  </div>
+);
+
+export default Root;
